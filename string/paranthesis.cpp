2ade51ac22e756168d77323f73c4f2676ae0d49d
Anshika Goyal <goyalanshika544@gmail.com>
2025-10-11 23:49:47 +0530

    Added paranthesis

---
+++ string/paranthesis.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+  string removeOuterParentheses(string s)
+  {
+    int n = s.size();
+    string ans = "";
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+      if (s[i] == '(')
+      {
+        count++;
+        if (count > 1)
+          ans = ans + s[i];
+      }
+      if (s[i] == ')')
+      {
+        count--;
+        if (count > 0)
+          ans = ans + s[i];
+      }
+    }
+    return ans;
+  }
+};
+int main()
+{
+  Solution sol;
+  string s = "((())())";
+  string ans = sol.removeOuterParentheses(s);
+  cout << "Resultant string is:" << ans << endl;
+}
